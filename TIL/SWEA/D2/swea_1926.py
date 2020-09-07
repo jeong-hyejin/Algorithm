14617d726ec7668eb4f5d484fe7b22d3c5499bd9
jeong-hyejin <9jung6@naver.com>
2020-09-07 23:36:08 +0900

    Add SWEA_1926

--- TIL/SWEA/D2/swea_1926.py
+++ TIL/SWEA/D2/swea_1926.py
@@ -1,14 +1,17 @@
 N = int(input())
-lst = ['3', '6', '9']
-for i in range(1, N+1):
-    for j in str(i):
-        if j in lst:
-            if lst.count(j) == 1:
-                i = '-'
-            elif lst.count(j) == 2:
-                i = '--'
-            elif lst.count(j) == 3:
-                i = '---'
-            # print('-', end=" ")
-
-    print(i, end=" ")
+lst = []
+while N != 0:
+    if N // 10 == 3 or N // 10 == 6 or N // 10 == 9:
+        if N % 10 == 3 or N % 10 == 6 or N % 10 == 9:
+            lst.append('-'*2)
+        else:
+            lst.append('-')
+        N -= 1
+    elif N % 10 == 3 or N % 10 == 6 or N % 10 == 9:
+        lst.append('-')
+        N -= 1
+    else:
+        lst.append((N // 10)* 10 + (N % 10))
+        N -= 1
+for i in lst[::-1]:
+    print(i,end=" ")
